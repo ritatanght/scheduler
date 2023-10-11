4dc1940c1109f9f64243b68e2452cc6277cb2f18
Rita Tang <doggymint@gmail.com>
2023-10-11 18:18:26 -0400

    Add test file for Application

---
+++ src/components/__tests__/Application.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { fireEvent, render, waitForElement } from "@testing-library/react";
+import Application from "../Application";
+
+describe("Appointment", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+    return waitForElement(() => getByText("Monday")).then(()=>{
+      fireEvent.click(getByText("Tuesday"))
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    })
+
+  });
+});
